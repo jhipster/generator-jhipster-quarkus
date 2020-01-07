41c90043cdb0ca6199771141cbb0f4f925f88107
Angelo Manganiello <angelo.mang@libero.it>
2020-01-07 11:05:14 +0100

    fix errors for lint

---
+++ generators/utils.js
@@ -0,0 +1,27 @@
+const _ = require('lodash');
+
+module.exports = {
+    buildEnumInfo
+};
+
+/**
+ * Build an enum object
+ * @param {any} field : entity field
+ * @param {string} angularAppName
+ * @param {string} packageName
+ * @param {string} clientRootFolder
+ */
+function buildEnumInfo(field, angularAppName, packageName, clientRootFolder) {
+    const fieldType = field.fieldType;
+    field.enumInstance = _.lowerFirst(fieldType);
+    const enumInfo = {
+        enumName: fieldType,
+        enumValues: field.fieldValues.split(',').join(', '),
+        enumInstance: field.enumInstance,
+        enums: field.fieldValues.replace(/\s/g, '').split(','),
+        angularAppName,
+        packageName,
+        clientRootFolder: clientRootFolder ? `${clientRootFolder}-` : ''
+    };
+    return enumInfo;
+}
