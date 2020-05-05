b4d5bd7f1418ed52150cb1ff0d47ab2ae6225974
danielpetisme <daniel.petisme@gmail.com>
2020-05-05 10:05:22 +0200

    Fix JWT testing

--- test/utils/expected-files.js
+++ test/utils/expected-files.js
@@ -12,7 +12,7 @@ const expectedFiles = {
         `${SERVER_MAIN_RES_DIR}application.properties`,
         `${SERVER_MAIN_RES_DIR}config/liquibase/master.xml`,
         `${SERVER_MAIN_RES_DIR}config/liquibase/changelog/00000000000000_initial_schema.xml`,
-        `${SERVER_MAIN_RES_DIR}META-INF/resources/privateKey.pem`,
+        `${SERVER_MAIN_RES_DIR}jwt/privateKey.pem`,
         `${SERVER_MAIN_RES_DIR}META-INF/resources/publicKey.pem`,
         `${SERVER_MAIN_RES_DIR}templates/mail/activationEmail.html`,
         `${SERVER_MAIN_RES_DIR}templates/mail/activationEmail.html`,
