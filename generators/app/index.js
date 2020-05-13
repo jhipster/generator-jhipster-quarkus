e4334da3cd803a00d3850db186e6de194363c1d1
danielpetisme <daniel.petisme@gmail.com>
2020-05-13 23:13:45 +0200

    Imprive start UX

--- generators/app/index.js
+++ generators/app/index.js
@@ -20,12 +20,24 @@ module.exports = class extends AppGenerator {
         const phaseFromQuarkus = {
             displayLogo() {
                 this.log('\n');
-                this.log(`${chalk.green('        ██╗')}${chalk.red(' ██╗   ██╗ ████████╗ ███████╗   ██████╗ ████████╗ ████████╗ ███████╗')}`);
-                this.log(`${chalk.green('        ██║')}${chalk.red(' ██║   ██║ ╚══██╔══╝ ██╔═══██╗ ██╔════╝ ╚══██╔══╝ ██╔═════╝ ██╔═══██╗')}`);
-                this.log(`${chalk.green('        ██║')}${chalk.red(' ████████║    ██║    ███████╔╝ ╚█████╗     ██║    ██████╗   ███████╔╝')}`);
-                this.log(`${chalk.green('  ██╗   ██║')}${chalk.red(' ██╔═══██║    ██║    ██╔════╝   ╚═══██╗    ██║    ██╔═══╝   ██╔══██║')}`);
-                this.log(`${chalk.green('  ╚██████╔╝')}${chalk.red(' ██║   ██║ ████████╗ ██║       ██████╔╝    ██║    ████████╗ ██║  ╚██╗')}`);
-                this.log(`${chalk.green('   ╚═════╝ ')}${chalk.red(' ╚═╝   ╚═╝ ╚═══════╝ ╚═╝       ╚═════╝     ╚═╝    ╚═══════╝ ╚═╝   ╚═╝')}`);
+                this.log(
+                    `${chalk.green('        ██╗')}${chalk.red(' ██╗   ██╗ ████████╗ ███████╗   ██████╗ ████████╗ ████████╗ ███████╗')}`
+                );
+                this.log(
+                    `${chalk.green('        ██║')}${chalk.red(' ██║   ██║ ╚══██╔══╝ ██╔═══██╗ ██╔════╝ ╚══██╔══╝ ██╔═════╝ ██╔═══██╗')}`
+                );
+                this.log(
+                    `${chalk.green('        ██║')}${chalk.red(' ████████║    ██║    ███████╔╝ ╚█████╗     ██║    ██████╗   ███████╔╝')}`
+                );
+                this.log(
+                    `${chalk.green('  ██╗   ██║')}${chalk.red(' ██╔═══██║    ██║    ██╔════╝   ╚═══██╗    ██║    ██╔═══╝   ██╔══██║')}`
+                );
+                this.log(
+                    `${chalk.green('  ╚██████╔╝')}${chalk.red(' ██║   ██║ ████████╗ ██║       ██████╔╝    ██║    ████████╗ ██║  ╚██╗')}`
+                );
+                this.log(
+                    `${chalk.green('   ╚═════╝ ')}${chalk.red(' ╚═╝   ╚═╝ ╚═══════╝ ╚═╝       ╚═════╝     ╚═╝    ╚═══════╝ ╚═╝   ╚═╝')}`
+                );
                 this.log('\n');
                 this.log(`${chalk.cyan('             ██████╗ ')}${chalk.red('██╗   ██╗ █████╗ ██████╗ ██╗  ██╗██╗   ██╗███████╗')}`);
                 this.log(`${chalk.cyan('            ██╔═══██╗')}${chalk.red('██║   ██║██╔══██╗██╔══██╗██║ ██╔╝██║   ██║██╔════╝')}`);
@@ -39,7 +51,9 @@ module.exports = class extends AppGenerator {
                 if (process.cwd() === this.getUserHome()) {
                     this.log(chalk.red.bold('\n️⚠️  WARNING ⚠️  You are in your HOME folder!'));
                     this.log(
-                        chalk.red('This can cause problems, you should always create a new directory and run the jhipster command from here.')
+                        chalk.red(
+                            'This can cause problems, you should always create a new directory and run the jhipster command from here.'
+                        )
                     );
                     this.log(chalk.white(`See the troubleshooting section at ${chalk.yellow('https://www.jhipster.tech/installation/')}`));
                 }
@@ -49,7 +63,9 @@ module.exports = class extends AppGenerator {
                     )
                 );
                 this.log(
-                    chalk.white(`  Documentation for creating an application is at ${chalk.yellow('https://github.com/jhipster/jhipster-quarkus')}`)
+                    chalk.white(
+                        `  Documentation for creating an application is at ${chalk.yellow('https://github.com/jhipster/jhipster-quarkus')}`
+                    )
                 );
                 this.log(
                     chalk.white(
