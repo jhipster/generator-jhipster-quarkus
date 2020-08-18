969107c6a8483ae62219cda9af75175169b79998
Igor Cherepov <inngvar@yandex.ru>
2020-08-18 22:01:24 +0300

    Feature/postgresql (#63)
    * Naming strategies for Hibernate to match names that used in common JHipster liquibase scripts
    
    * Naming strategies for Hibernate to match names that used in common JHipster liquibase scripts
    Use sequence generator for User if postgresql is used as database

--- generators/server/files.js
+++ generators/server/files.js
@@ -112,6 +112,14 @@ const serverFiles = {
                 {
                     file: 'package/config/JsonbConfiguration.java',
                     renameTo: generator => `${generator.javaDir}config/JsonbConfiguration.java`
+                },
+                {
+                    file: 'package/config/hibernate/JHipsterCompatibleImplicitNamingStrategy.java',
+                    renameTo: generator => `${generator.javaDir}config/hibernate/JHipsterCompatibleImplicitNamingStrategy.java`
+                },
+                {
+                    file: 'package/config/hibernate/JHipsterCompatiblePhysicalNamingStrategy.java',
+                    renameTo: generator => `${generator.javaDir}config/hibernate/JHipsterCompatiblePhysicalNamingStrategy.java`
                 }
             ]
         }
