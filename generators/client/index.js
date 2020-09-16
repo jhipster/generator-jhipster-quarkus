8cb81b6893393cb2fbadb8672644971af25a6ddf
Anthony Viard <anth.viard@gmail.com>
2020-09-16 16:40:47 +0200

    Init Quarkus health check admin page for Angular

---
+++ generators/client/index.js
@@ -0,0 +1,64 @@
+/* eslint-disable consistent-return */
+const chalk = require('chalk');
+const ClientGenerator = require('generator-jhipster/generators/client');
+const constants = require('generator-jhipster/generators/generator-constants');
+const writeAngularFiles = require('./files-angular').writeFiles;
+
+const { ANGULAR } = constants.SUPPORTED_CLIENT_FRAMEWORKS;
+
+module.exports = class extends ClientGenerator {
+    constructor(args, opts) {
+        super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
+
+        const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
+
+        if (!jhContext) {
+            this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprint quarkus')}`);
+        }
+
+        this.configOptions = jhContext.configOptions || {};
+
+        // This sets up options for this sub generator and is being reused from JHipster
+        jhContext.setupClientOptions(this, jhContext);
+    }
+
+    get initializing() {
+        return super._initializing();
+    }
+
+    get prompting() {
+        return super._prompting();
+    }
+
+    get configuring() {
+        // Here we are not overriding this phase and hence its being handled by JHipster
+        return super._configuring();
+    }
+
+    get default() {
+        // Here we are not overriding this phase and hence its being handled by JHipster
+        return super._default();
+    }
+
+    get writing() {
+        const phaseFromJHipster = super._writing();
+        const phaseFromQuarkus = {
+            writeQuarkusFiles() {
+                if (this.skipClient) return;
+                if (this.clientFramework === ANGULAR) {
+                    return writeAngularFiles.call(this);
+                }
+            }
+        };
+        return { ...phaseFromJHipster, ...phaseFromQuarkus };
+    }
+
+    get install() {
+        // Here we are not overriding this phase and hence its being handled by JHipster
+        return super._install();
+    }
+
+    get end() {
+        return super._end();
+    }
+};
