f474293cdd20e66d51a5dc1655dd39c865b4a977
Marcelo Shima <marceloshima@gmail.com>
2021-08-13 18:41:04 -0300

    Adjusts to app generator.

--- generators/app/index.js
+++ generators/app/index.js
@@ -98,48 +98,11 @@ module.exports = class extends AppGenerator {
     }
 
     get composing() {
-        return super._composing();
-        // this.composeWithJHipster(GENERATOR_COMMON, true);
-        // const phaseFromJHipster = super._composing();
-        // const phaseFromQuarkus = {
-        //     composeServer() {
-        //         if (this.skipServer) return;
-        //         const options = this.options;
-        //         const configOptions = this.configOptions;
-        //
-        //         this.composeWith(require.resolve('../server'), {
-        //             ...options,
-        //             configOptions,
-        //             'client-hook': !this.skipClient,
-        //             debug: this.isDebugEnabled,
-        //         });
-        //     },
-        //
-        //     composeClient() {
-        //         if (this.skipClient) return;
-        //         const options = this.options;
-        //         const configOptions = this.configOptions;
-        //
-        //         this.composeWith(require.resolve('../client'), {
-        //             ...options,
-        //             configOptions,
-        //             debug: this.isDebugEnabled,
-        //         });
-        //     },
-        //
-        //     composeCommon() {
-        //         const options = this.options;
-        //         const configOptions = this.configOptions;
-        //
-        //         this.composeWith(require.resolve('../common'), {
-        //             ...options,
-        //             'client-hook': !this.skipClient,
-        //             configOptions,
-        //             debug: this.isDebugEnabled,
-        //         });
-        //     },
-        // };
-        // return { ...phaseFromJHipster, ...phaseFromQuarkus };
+        return {
+            ...super._composing(),
+            askForTestOpts: undefined,
+            askForMoreModules: undefined,
+        };
     }
 
     get loading() {
@@ -151,12 +114,7 @@ module.exports = class extends AppGenerator {
     }
 
     get default() {
-        const phaseFromJHipster = super._default();
-        const phaseFromQuarkus = {
-            askForTestOpts: undefined,
-            askForMoreModules: undefined,
-        };
-        return { ...phaseFromJHipster, ...phaseFromQuarkus };
+        return super._default();
     }
 
     get writing() {
