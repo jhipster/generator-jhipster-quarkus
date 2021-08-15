3d79024f0e5432daa9c8638ad771521a8e65c9b9
Marcelo Shima <marceloshima@gmail.com>
2021-08-15 12:00:09 -0300

    Update angular tests.

--- test/client.spec.js
+++ test/client.spec.js
@@ -10,19 +10,10 @@ describe('Subgenerator client of quarkus JHipster blueprint', () => {
         before(buildClientGeneratorContext());
 
         it('Angular health check files contain expected content', () => {
-            assert.fileContent(
-                `${ANGULAR_DIR}admin/health/health.component.html`,
-                '<tr *ngFor="let componentHealth of health.checks | keys">'
-            );
-            assert.fileContent(
-                `${ANGULAR_DIR}admin/health/health.service.ts`,
-                `
-export interface HealthDetails {
-  status: HealthStatus;
-  details: any;
-}
-`
-            );
+            assert.noFileContent(`${ANGULAR_DIR}admin/health/health.component.html`, 'components');
+            assert.noFileContent(`${ANGULAR_DIR}admin/health/health.component.spec.ts`, 'components');
+            assert.noFileContent(`${ANGULAR_DIR}admin/health/health.model.ts`, 'components');
+            assert.noFileContent(`${ANGULAR_DIR}admin/health/health.service.ts`, 'components');
         });
 
         it('Angular configuration files contain expected content', () => {
