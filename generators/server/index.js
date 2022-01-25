5fa53599cf83b4567920131dd848d4f80df7cf35
Sergey Averkiev <exanimoso@yandex.ru>
2022-01-25 17:45:46 +0300

    Set node version to 12.20.0 in package-lock.json

--- generators/server/index.js
+++ generators/server/index.js
@@ -38,7 +38,6 @@ module.exports = class extends ServerGenerator {
             },
         };
         return { ...phaseFromJHipster, ...readCustomConfiguration, ...phaseFromQuarkus };
-        //return { ...phaseFromJHipster, ...phaseFromQuarkus };
     }
 
     get prompting() {
