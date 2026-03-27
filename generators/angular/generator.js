92d27cc1c65a040e5bc08e92a4622771784071b9
Marcelo Shima <marceloshima@gmail.com>
2026-03-27 19:39:40 -0300

    Rename configuration.component.html to configuration.html

--- generators/angular/generator.js
+++ generators/angular/generator.js
@@ -8,7 +8,7 @@ export default class extends BaseApplicationGenerator {
     get [BaseApplicationGenerator.POST_WRITING]() {
         return this.asPostWritingTaskGroup({
             async customize({ application }) {
-                this.editFile(`${application.clientSrcDir}app/admin/configuration/configuration.component.html`, content =>
+                this.editFile(`${application.clientSrcDir}app/admin/configuration/configuration.html`, content =>
                     content.replace(
                         '<h3 id="spring-configuration">Spring configuration</h3>',
                         '<h3 id="Quarkus-configuration">Quarkus configuration</h3>',
