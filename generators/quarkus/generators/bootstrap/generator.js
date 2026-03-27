9eb17522ac78ec8aa2b8f6c58f4bec5c6ed40435
Marcelo Shima <marceloshima@gmail.com>
2026-03-27 18:28:31 -0300

    replace bootstrap-application with quarkus:bootstrap

--- generators/quarkus/generators/bootstrap/generator.js
+++ generators/quarkus/generators/bootstrap/generator.js
@@ -1,20 +1,27 @@
 import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
+
 import { CACHE_EXPIRE_AFTER_WRITE, CACHE_MAXIMUM_SIZE } from '../constants.js';
 
 export default class extends BaseApplicationGenerator {
     constructor(args, opts, features) {
-        super(args, opts, { ...features, sbsBlueprint: true });
+        super(args, opts, {
+            ...features,
+
+            sbsBlueprint: true,
+        });
     }
 
     get [BaseApplicationGenerator.LOADING]() {
         return this.asLoadingTaskGroup({
-            async loadingTemplateTask({ application }) {
-                application.backendType = 'Quarkus';
-                application.backendTypeJavaAny = true;
-                application.CACHE_MAXIMUM_SIZE = CACHE_MAXIMUM_SIZE;
-                application.CACHE_EXPIRE_AFTER_WRITE = CACHE_EXPIRE_AFTER_WRITE;
-                application.jhipsterQuarkusVersion = undefined;
-                application.messageBrokerAny ??= undefined;
+            async loadingTemplateTask({ applicationDefaults }) {
+                applicationDefaults({
+                    backendType: 'Quarkus',
+                    backendTypeJavaAny: true,
+                    CACHE_MAXIMUM_SIZE,
+                    CACHE_EXPIRE_AFTER_WRITE,
+                    jhipsterQuarkusVersion: undefined,
+                    messageBrokerAny: undefined,
+                });
             },
         });
     }
--- generators/quarkus/generators/bootstrap/generator.spec.js
+++ generators/quarkus/generators/bootstrap/generator.spec.js
@@ -2,21 +2,21 @@ import { beforeAll, describe, expect, it } from 'vitest';
 
 import { defaultHelpers as helpers, result } from 'generator-jhipster/testing';
 
-const SUB_GENERATOR = 'bootstrap-application';
-const BLUEPRINT_NAMESPACE = `jhipster:${SUB_GENERATOR}`;
+const SUB_GENERATOR = 'quarkus:bootstrap';
+const SUB_GENERATOR_NAMESPACE = `jhipster-quarkus:${SUB_GENERATOR}`;
 
-describe('SubGenerator bootstrap-application of quarkus JHipster blueprint', () => {
+describe('SubGenerator quarkus:bootstrap of quarkus JHipster blueprint', () => {
     describe('run', () => {
         beforeAll(async function () {
             await helpers
-                .run(BLUEPRINT_NAMESPACE)
+                .run(SUB_GENERATOR_NAMESPACE)
                 .withJHipsterConfig()
                 .withOptions({
                     ignoreNeedlesError: true,
-                    blueprint: ['quarkus'],
                 })
-                .withJHipsterLookup()
-                .withParentBlueprintLookup();
+                .withJHipsterGenerators()
+                .withConfiguredBlueprint()
+                .withBlueprintConfig();
         });
 
         it('should succeed', () => {
